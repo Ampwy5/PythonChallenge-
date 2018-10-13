a1b162709a1a6793bd276e2705dd389a860e8e5e
Addison Pietroburgo <addisonpietroburgo@Addisons-MacBook-Air.local>
2018-10-13 14:47:14 -0500

    Ran both scripts successfully and printed results to txt files saved within respective folders.

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -10,8 +10,11 @@ totalVotes = 0
 liTotal = 0 
 khanTotal = 0 
 correyTotal = 0 
-otooleyTotal = 0 
+otooleyTotal = 0
+winningVoteCount = 0 
 
+#create list of candidates
+candidateList = ["Li", "Khan", "Correy", "O'Tooley"]
 
 #reading in CSV
 with open(electioncsv, 'r') as csvfile:
@@ -22,18 +25,28 @@ with open(electioncsv, 'r') as csvfile:
 #calculate Total Votes by adding to 0 all rows iterated over 
         totalVotes += 1
         #calculate total for candidate Li and percent 
-        if (row[2]) = 'Li':
+        if (row[2]) == 'Li':
             liTotal += 1
             liPercent = liTotal/totalVotes * 100
-        if (row[2]) = 'Khan':
+        if (row[2]) == 'Khan':
             khanTotal += 1 
             khanPercent = khanTotal/totalVotes * 100 
-        if (row[2]) = 'Correy':
+        if (row[2]) == 'Correy':
             correyTotal += 1 
             correyPercent = correyTotal/totalVotes * 100 
-        if (row[2]) = "O'Tooley" : 
+        if (row[2]) == "O'Tooley": 
             otooleyTotal += 1 
-            otooleyPercent = otooleyTotal/totalVotes * 100  
+            otooleyPercent = otooleyTotal/totalVotes * 100 
+        #calculate winner  
+        if otooleyTotal > correyTotal and otooleyTotal > liTotal and otooleyTotal > khanTotal:
+            electionWinner = "O'Tooley"
+        if khanTotal > correyTotal and khanTotal > liTotal and khanTotal > otooleyTotal:
+            electionWinner = "Khan"
+        if liTotal > correyTotal and liTotal > otooleyTotal and liTotal > khanTotal:
+            electionWinner = "Li"
+        if correyTotal > otooleyTotal and correyTotal > khanTotal and correyTotal > liTotal: 
+            electionWinner = "Correy" 
+        
 
 
 #defining Analysis and printing 
@@ -46,6 +59,9 @@ f"Khan: {khanTotal} (%{khanPercent}) \n"
 f"Correy: {correyTotal} (%{correyPercent}) \n"
 f"Li: {liTotal} (%{liPercent}) \n"
 f"O'Tooley: {otooleyTotal} (%{otooleyPercent}) \n"
+f"-------------------------- \n"
+f"WINNER: {electionWinner} \n"
+f"-------------------------- \n"
 )
 print(electionResults)
 
@@ -59,6 +75,9 @@ f"Khan: {khanTotal} (%{khanPercent}) \n"
 f"Correy: {correyTotal} (%{correyPercent}) \n"
 f"Li: {liTotal} (%{liPercent}) \n"
 f"O'Tooley: {otooleyTotal} (%{otooleyPercent}) \n"
+f"-------------------------- \n"
+f"WINNER: {electionWinner} \n"
+f"-------------------------- \n"
 )
 
 file.close()
